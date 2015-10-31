3bee8bd63663777433dd34419faf5037b0129cf3
John Moons <jmoons@John-Moonss-MacBook-Pro.local>
2015-10-31 19:42:24 -0400

    Added first pieces of Robot class and associated tests

--- robot.rb
+++ robot.rb
@@ -1,6 +1,21 @@
 class Robot
-  def initialize(args)
+  attr_accessor :name, :position
 
+  INITIAL_STARTING_POSITION = 1
+  FINAL_ENDING_POSITION     = 100
+  POSITION_MOVE_DISTANCE    = 1
+
+  def initialize(name)
+    @position = INITIAL_STARTING_POSITION
+    @name     = name
+  end
+
+  def move_forward
+    @position += 1 unless @position == FINAL_ENDING_POSITION
+  end
+
+  def move_backward
+    @position -= 1 unless @position == INITIAL_STARTING_POSITION
   end
 
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -1,3 +1,40 @@
 require_relative '../robot'
 
-require 'yaml'
+describe "#Robot" do
+
+  before :each do
+    @test_robot = Robot.new("Batman")
+  end
+
+  it "must be given a name" do
+    expect(@test_robot.name).to eq("Batman")
+  end
+
+  it "starting position is 1" do
+    expect(@test_robot.position).to eq(Robot::INITIAL_STARTING_POSITION)
+  end
+
+  it "#move_forward advances the robot by one only if not at position 100" do
+    current_position = @test_robot.position
+    @test_robot.move_forward
+    expect(@test_robot.position).to eq(current_position + Robot::POSITION_MOVE_DISTANCE)
+  end
+
+  it "#move_forward does not advance the robot if at the end of the hallway" do
+    @test_robot.position  = Robot::FINAL_ENDING_POSITION
+    @test_robot.move_forward
+    expect(@test_robot.position).to eq(Robot::FINAL_ENDING_POSITION)
+  end
+
+  it "#move_backward retreats the robot by one" do
+    current_position      = 50
+    @test_robot.position  = current_position
+    @test_robot.move_backward
+    expect(@test_robot.position).to eq(current_position - Robot::POSITION_MOVE_DISTANCE)
+  end
+
+  it "#move_backward does not advance the robot if at the beginning of the hallway" do
+    @test_robot.move_backward
+    expect(@test_robot.position).to eq(Robot::INITIAL_STARTING_POSITION)
+  end
+end
