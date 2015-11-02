d99b2c22c49bb9ecf141294cc66d5c6e073f20d7
John Moons <j.moons@realdigitalmedia.com>
2015-11-02 17:34:21 -0500

    Adding support for independent vs dependent test cases

--- test_conductor.rb
+++ test_conductor.rb
@@ -11,10 +11,41 @@ class TestConductor
   def initialize(input_file)
     @input_file = input_file
     @test_cases = []
+
     initialize_test_cases
+    # execute_test_cases_independent
+    # execute_test_cases_dependent
+
+  end
+
+  def execute_test_cases_independent
+    @test_cases.each do | test_case |
+      while ( !(test_case.complete?) )
+        test_case.perform_test_case_step_independent
+      end
+    end
+
+    print_test_case_results
+  end
+
+  def execute_test_cases_dependent
+    @test_cases.each do | test_case |
+      while ( !(test_case.complete?) )
+        test_case.perform_test_case_step_dependent
+      end
+    end
+
+    print_test_case_results
   end
 
   private
+
+  def print_test_case_results
+    @test_cases.each_with_index do | test_case, test_case_index |
+      puts "Case ##{test_case_index + 1}: #{test_case.time}"
+    end
+    puts "======================"
+  end
 
   def initialize_test_cases
     @input_file.each_line do |input_file_line|
@@ -25,3 +56,6 @@ class TestConductor
   end
 
 end
+
+# TestConductor.new( File.open("spec/test_input.txt", "r")).execute_test_cases_dependent
+# TestConductor.new( File.open("spec/test_input.txt", "r")).execute_test_cases_independent
