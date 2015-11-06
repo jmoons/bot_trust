d41e0aacae7b9626f9a4fbfe1341907938f88f68
John Moons <j.moons@realdigitalmedia.com>
2015-11-06 15:27:03 -0500

    Making the test conductor a bit DRY'er

--- test_conductor.rb
+++ test_conductor.rb
@@ -9,36 +9,36 @@ class TestConductor
   attr_reader :test_cases
 
   def initialize(input_file)
-    @input_file = input_file
-    @test_cases = []
+    @input_file               = input_file
+    @test_cases               = []
 
     initialize_test_cases
-    # execute_test_cases_independent
-    # execute_test_cases_dependent
 
   end
 
   def execute_test_cases_independent
-    @test_cases.each do | test_case |
-      while ( !(test_case.complete?) )
-        test_case.perform_test_case_step_independent
-      end
-    end
 
-    print_test_case_results
+    execute_test_cases(:independent)
+
   end
 
   def execute_test_cases_dependent
+
+    execute_test_cases(:dependent)
+
+  end
+
+  private
+
+  def execute_test_cases(execution_mode = :dependent)
     @test_cases.each do | test_case |
       while ( !(test_case.complete?) )
-        test_case.perform_test_case_step_dependent
+        test_case.send( ("perform_test_case_step_" << execution_mode.to_s).to_sym )
       end
     end
 
     print_test_case_results
   end
-
-  private
 
   def print_test_case_results
     @test_cases.each_with_index do | test_case, test_case_index |
@@ -57,5 +57,5 @@ class TestConductor
 
 end
 
-# TestConductor.new( File.open("spec/test_input.txt", "r")).execute_test_cases_dependent
-# TestConductor.new( File.open("spec/test_input.txt", "r")).execute_test_cases_independent
+TestConductor.new( File.open("spec/test_input.txt", "r") ).execute_test_cases_dependent
+TestConductor.new( File.open("spec/test_input.txt", "r") ).execute_test_cases_independent
